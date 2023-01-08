d6b77f0a7df35db31cb6df2dbfe18499f3126a68
jun <ggprgrkjh@naver.com>
2023-01-08 22:33:28 +0900

    [JH] solve DAY4

---
+++ JH/DAY4/2741.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main(){
+    cin >> n;
+
+    for (int i = 1; i <=n ; ++i) {
+        cout << i <<"\n";
+    }
+
+}
+
+
+
+
+
---
+++ JH/DAY4/2742.cpp
@@ -0,0 +1,20 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main(){
+    cin >> n;
+
+    for (int i = n; i >=1 ; --i) {
+        cout << i <<"\n";
+    }
+
+}
+
+
+
+
+
