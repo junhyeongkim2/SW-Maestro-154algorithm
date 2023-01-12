4c55dc648ac7d047d363fff7c397cc7b5ee780d2
June <ziyun1612@naver.com>
2023-01-12 10:25:53 +0900

    DAY3 + DAY4 : DP

--- JY/DAY4/BOJ_2193.java
+++ JY/DAY4/BOJ_2193.java
@@ -8,14 +8,11 @@ public class BOJ_2193 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
-        int[] dp = new int[num+1]; // 0~9 까지 경우의 수
-        dp[0] = 1;
+        long[] dp = new long[num+1]; // 0~9 까지 경우의 수
+        dp[0] = 0;
         dp[1] = 1;
-        dp[2] = 1;
-        dp[3] = 2;
-        dp[4] = 5;
 
-        for(int i = 5; i<= num; i++){
+        for(int i = 2; i<= num; i++){
             dp[i] = dp[i-2] + dp[i-1];
         }
         System.out.println(dp[num]);
