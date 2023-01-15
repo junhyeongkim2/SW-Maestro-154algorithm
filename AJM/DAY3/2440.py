c64b4602492c16334acccba46edf66cf775dfe88
102sae <95170874+102sae@users.noreply.github.com>
2023-01-15 18:48:15 +0900

    DAY3

---
+++ AJM/DAY3/2440.py
@@ -0,0 +1,6 @@
+#별 찍기 -3
+
+n = int(input())
+
+for i in range(n):
+    print("*"*(n-i))
---
+++ AJM/DAY3/2522.py
@@ -0,0 +1,8 @@
+#별 찍기 -12
+
+n = int(input())
+
+for i in range(1,n+1):
+    print(" "*(n-i)+"*"*(i))
+for i in range(1,n):
+    print(" "*(i)+"*"*(n-i))
