c65ae20d7655711677310350b88931b21bb922b1
happine2s <happine2s@sookmyung.ac.kr>
2023-01-15 22:13:00 +0900

    [Day3] DP

---
+++ SY/DP/2579.py
@@ -0,0 +1,13 @@
+n=int(input())
+li=[int(input()) for _ in range(n)]
+dp=[0]*n
+dp[0]=li[0]
+if n>1:
+	dp[1]=li[0]+li[1]
+if n>2:
+	dp[2]=max(li[0]+li[2],li[1]+li[2])
+
+for i in range(3,n):
+	dp[i]=max(dp[i-3]+li[i-1]+li[i], dp[i-2]+li[i])
+
+print(dp[n-1])
