59e505dc46c76045556dd9274855ab12181ba58d
Minhye Kang <alsgp9898@naver.com>
2023-01-16 15:06:45 +0900

    refactor: reduce time complexity

--- MH/DAY7/10808.js
+++ MH/DAY7/10808.js
@@ -1,16 +1,11 @@
 const path = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
 let input = require('fs').readFileSync(path).toString().trim();
 
-let arr = Array.from(26);
-let num = 97;
-
-for(let i=0; i<26; i++){
-  arr[i] = String.fromCharCode(num);
-  num++;
-}
+let arr = 'abcdefghijklmnopqrstuvwxyz';
+let answer = [];
 
 for(let j=0; j<26; j++){
-  arr[j] = input.split(arr[j]).length-1
+  answer[j] = input.split(arr[j]).length-1
 }
 
-console.log(arr.join(' '))
+console.log(answer.join(' '))
