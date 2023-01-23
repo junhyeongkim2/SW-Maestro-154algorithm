088ab7db485aa9957168657ff14477b6d7367c39
DAY <ekdud116211@naver.com>
2023-01-24 01:09:25 +0900

    solve DAY16 problem

---
+++ DY/DAY16/11022.py
@@ -0,0 +1,6 @@
+import sys
+read = sys.stdin.readline
+
+for i in range(int(read().strip())):
+    a, b = map(int, read().split())
+    print(f'Case #{i+1}: {a} + {b} = {a + b}')
---
+++ DY/DAY16/11720.py
@@ -0,0 +1,3 @@
+N = input()
+nums = input()
+print(sum(map(int, nums)))
