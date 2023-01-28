1537d9da8d0d18c98eaad210417690e0eb700c06
Yu-Rim Seok <96058798+syr96@users.noreply.github.com>
2023-01-28 20:22:12 +0900

    Create 2741.java

---
+++ YR/Day04/2741.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int N = scan.nextInt();
+        
+        for(int i = 0; i < N; i++) {
+            System.out.println(i + 1);
+        }
+    }
+}
