8ea71b345b62cc49bc7c35ebd540ebb87a0f2c1b
박준용 <jo101044@naver.com>
2023-01-30 22:09:01 +0900

    Day16 : DFS/BFS
    프로그래머스)네트워크.java

---
+++ PJY/DAY16/프로그래머스)네트워크.java
@@ -0,0 +1,25 @@
+class Solution {
+    boolean[] arrived;
+
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        arrived = new boolean[computers.length];
+        for (int i = 0; i < computers.length; i++) {
+            if (!arrived[i]) {
+                network(i, computers);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public void network(int n, int[][] computers) {
+        for (int i = 0; i < computers[n].length; i++) {
+            if (!arrived[i] && computers[n][i] == 1) {
+                arrived[i] = true;
+                network(i, computers);
+            }
+        }
+    }
+}
