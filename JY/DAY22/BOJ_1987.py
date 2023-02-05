d3ab7b377c97e350a4d228d48804ac7563949fdc
June <ziyun1612@naver.com>
2023-02-05 20:57:36 +0900

    DAY22 : 완전탐색

---
+++ JY/DAY22/BOJ_1987.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+table = []
+for _ in range(A):
+    table.append(input().rstrip())
+
+    
+res = []
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+visited = [["" for _ in range(B)]  for _ in range(A)]
+
+def bfs():
+    global ans
+    qu = deque()
+    qu.append([0, 0, table[0][0]])
+    ans = 0
+    while qu:
+        x, y, v = qu.popleft()
+        visited[x][y] = True
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < A and 0 <= ny < B and table[nx][ny] not in v:
+                if visited[nx][ny] != v: #그냥 참거짓으로 하면 다시 돌아올 수 없다. -> 그냥 그동안 왔던 경로를 저장하자 
+                    visited[nx][ny] = v
+                    qu.append([nx, ny, v+table[nx][ny]])
+                    ans = max(len(v)+1, ans)
+    return ans
+                
+
+
+print(bfs())
+                    
+                
+        
+
+
+
+    
