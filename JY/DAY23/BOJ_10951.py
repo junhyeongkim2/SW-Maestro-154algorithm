0692b3a171f3206995e9139868d010b7d1fa839c
june0216 <ziyun1612@ewhain.net>
2023-02-06 22:54:20 +0900

    DAY 23 : 입출력

---
+++ JY/DAY23/BOJ_10951.py
@@ -0,0 +1,6 @@
+while True:
+    try:
+        A, B = map(int, input().split())
+        print(A+B)
+    except:
+        break
---
+++ JY/DAY23/BOJ_10952.py
@@ -0,0 +1,5 @@
+while True:
+    A, B = map(int, input().split())
+    if A == 0 and B == 0:
+        break
+    print(A + B)
