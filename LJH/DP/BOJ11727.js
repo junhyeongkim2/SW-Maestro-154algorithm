c7eae6e9f900400b1908d179a4ff2d421309feb5
JaeHye0k <ahhpc2012@naver.com>
2024-01-27 20:24:41 +0900

    dp[i] = dp[i-1] + dp[i-2]*2

---
+++ LJH/DP/BOJ11727.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./LJH/input.txt";
+let n = Number(fs.readFileSync(filePath).toString());
+let dp = {
+    1: 1,
+    2: 3,
+};
+for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+}
+
+console.log(dp[n]);
