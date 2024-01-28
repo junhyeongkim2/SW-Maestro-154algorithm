47de3a25df4a110be3023aecb4fc70e43db5116c
JaeHye0k <ahhpc2012@naver.com>
2024-01-28 09:43:46 +0900

    solve

--- LJH/DP/BOJ10844.js
+++ LJH/DP/BOJ10844.js
@@ -2,9 +2,18 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./LJH/input.txt";
 let n = Number(fs.readFileSync(filePath).toString());
 let dp = {
-    1: 9,
+    1: [0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 };
+const mod = 10 ** 9;
 for (let i = 2; i <= n; i++) {
-    dp[i] = i % 2 === 0 ? (dp[i - 1] * 2 - 1) % 10 ** 9 : (dp[i - 1] * 2 - 2) % 10 ** 9;
+    dp[i] = Array(10).fill(0);
+    for (let j = 0; j <= 9; j++) {
+        if (j === 0) dp[i][j] = dp[i - 1][j + 1];
+        else if (j === 9) dp[i][j] = dp[i - 1][j - 1];
+        else dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
+        dp[i][j] %= mod;
+    }
 }
-console.log(dp[n]);
+
+let result = dp[n].reduce((acc, cur) => (acc + cur) % mod);
+console.log(result);
